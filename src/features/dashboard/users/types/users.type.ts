ba535873b72919c3eea1461d3a729b238fbcbedf
nrmadi02 <ahmadinur2202@gmail.com>
2025-12-25 23:45:39 +0800

    feat(dashboard): add users feature pages and types

---
+++ src/features/dashboard/users/types/users.type.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+	id: string;
+	username: string;
+	fullName: string;
+	email: string;
+	role: string;
+	emailVerified: boolean;
+	createdAt: string;
+	updatedAt: string;
+}
