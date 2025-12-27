b2ab78c8ff4955f14d169afd74593ba6f0ac6303
nrmadi02 <ahmadinur2202@gmail.com>
2025-12-27 15:39:07 +0800

    chore(router): update react-router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
-	ssr: false,
+	ssr: true,
 	appDirectory: "src",
 	future: {
 		v8_middleware: true,
